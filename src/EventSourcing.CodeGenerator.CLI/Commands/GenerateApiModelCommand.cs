e02334885a5d3f799d25b109cf9235f02030a1a7
quinntynebrown@gmail.com <quinntynebrown@gmail.com>
2018-03-04 05:10:31 -0500

    initial

---
+++ src/EventSourcing.CodeGenerator.CLI/Commands/GenerateApiModelCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace EventSourcing.CodeGenerator.CLI.Commands
+{
+    public class GenerateApiModelCommand
+    {
+        public class Options
+        {
+
+        }
+
+        public class Request: IRequest
+        {
+            public Request(string[] args)
+            {
+
+            }
+        }
+
+        public class Handler : IRequestHandler<Request>
+        {
+            public Task Handle(Request message, CancellationToken cancellationToken)
+            {
+                Console.WriteLine("works?");
+
+                Console.ReadLine();
+
+                return Task.CompletedTask;
+            }
+        }
+    }
+}
