48c9b3be090c4f32d97cc9bcff9edbc83b03b84b
quinntynebrown@gmail.com <quinntynebrown@gmail.com>
2018-07-25 05:20:34 -0400

    feature

--- src/Quinntyne.Schematics.CLI/Features/EventSourcing/GenerateFeatureCommand.cs
+++ src/Quinntyne.Schematics.CLI/Features/EventSourcing/GenerateFeatureCommand.cs
@@ -36,12 +36,11 @@ namespace Quinntyne.Schematics.CLI.Features.EventSourcing
             public async Task Handle(Request request, CancellationToken cancellationToken)
             {
                 await _mediator.Send(new GenerateApiModelCommand.Request(request.Options));
-                await _mediator.Send(new GenerateRemoveCommand.Request(request.Options));
-                await _mediator.Send(new GenerateRemovedEventCommand.Request(request.Options));
+                await _mediator.Send(new GenerateRemoveCommand.Request(request.Options));             
                 await _mediator.Send(new GenerateGetByIdQueryCommand.Request(request.Options));
                 await _mediator.Send(new GenerateGetQueryCommand.Request(request.Options));
-                await _mediator.Send(new GenerateSaveCommand.Request(request.Options));
-                await _mediator.Send(new GenerateSavedEventCommand.Request(request.Options));
+                await _mediator.Send(new GenerateCreateCommand.Request(request.Options));
+                await _mediator.Send(new GenerateUpdateCommand.Request(request.Options));
                 await _mediator.Send(new GenerateControllerCommand.Request(request.Options));
 
                 await Task.CompletedTask;
