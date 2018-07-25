34f413e975a42f25c81fdc4e46cd8b0e2614a4b6
quinntynebrown@gmail.com <quinntynebrown@gmail.com>
2018-07-25 19:22:20 -0400

    files

---
+++ src/Quinntyne.Schematics.Utilities/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Quinntyne.Schematics.Utilities
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
